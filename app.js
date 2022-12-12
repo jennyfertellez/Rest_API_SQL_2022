9693c53b456a56f02d70fb155fda73cc3d4dc72e
jennyfertellez <jennyfertellez@gmail.com>
2022-12-11 22:01:59 -0800

    sequelize authenticate

--- app.js
+++ app.js
@@ -1,33 +1,51 @@
 'use strict';
 
-// load modules
+//Load Modules
 const express = require('express');
 const morgan = require('morgan');
+const { Sequelize } = require('sequelize');
 
-// variable to enable global error logging
+//Sequelize autheticate to test the databade connection
+console.log("Testing the connection to the database...");
+
+(async () => {
+  await Sequelize.sync();
+  
+  try {
+    //Test the connection to the database
+    await sequelize.autheticate();
+    console.log("Connection to the database successful!");
+  } catch(error) {
+    console.log("Error connection to the database: ", error);
+  }
+})();
+
+
+
+//Variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
 
-// create the Express app
+//Create the Express app
 const app = express();
 
-// setup morgan which gives us http request logging
+//Setup morgan which gives us http request logging
 app.use(morgan('dev'));
 
-// setup a friendly greeting for the root route
+//Setup a friendly greeting for the root route
 app.get('/', (req, res) => {
   res.json({
     message: 'Welcome to the REST API project!',
   });
 });
 
-// send 404 if no other route matched
+//Send 404 if no other route matched
 app.use((req, res) => {
   res.status(404).json({
     message: 'Route Not Found',
   });
 });
 
-// setup a global error handler
+//Setup a global error handler
 app.use((err, req, res, next) => {
   if (enableGlobalErrorLogging) {
     console.error(`Global error handler: ${JSON.stringify(err.stack)}`);
@@ -39,10 +57,10 @@ app.use((err, req, res, next) => {
   });
 });
 
-// set our port
+//Set our port
 app.set('port', process.env.PORT || 5000);
 
-// start listening on our port
+//Start listening on our port
 const server = app.listen(app.get('port'), () => {
   console.log(`Express server is listening on port ${server.address().port}`);
 });
