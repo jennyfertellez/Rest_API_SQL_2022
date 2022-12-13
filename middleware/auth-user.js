2712d14e8ec273a2fa6907d29fb5b94d96acacaf
jennyfertellez <jennyfertellez@gmail.com>
2022-12-13 00:47:04 -0800

    updated authorized user

---
+++ middleware/auth-user.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const auth = require('basic-auth');
+const { User } = require('../models');
+const bcrypt = require('bcrypt');
+const e = require('express');
+
+//Middleware to authenticate teh request using Basic Authorization
+exports.authenticateUser = async (req, res, next) => {
+    //Store the message to display
+    let message;
+    //Parse the user's credentials from the Authorication header
+    const credentials = auth(req);
+
+    //If the user's credentials are available
+    if (credentials) {
+        const user = await User.findOne({ where: {emailAddress: credentials.name} });
+    //If a user was successfully retrived from the data store
+        if (user) {
+            const authenticate = bcrypt.compareSync(credentials.pass, user.password);
+        
+    //If the password match
+            if (authenticated) {
+                console.log(`Authentication successful for email address: ${user.emailAddress}`)
+
+                //Store the user email address on the Request object
+                req.currentUser = user;
+            
+            } else  {
+                message = `Authetication failure for user's email address: ${user.emailAddress}`;
+            } 
+        } else {
+                message = `User not found for name: ${credentials.name}`;
+            }
+        } else {
+                message = 'Auth header not found';
+            }
+
+    //If user authentication failed
+    if (message) {
+        console.warn(message);
+        res.status(401).json({ message: 'Access Denied' });
+    }
+    
+    //If the user authentication succeeded
+    next();
+}
