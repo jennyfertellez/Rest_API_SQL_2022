c0c8c78cc77dc0c3f039010d9ef4e66ea19faeb9
jennyfertellez <jennyfertellez@gmail.com>
2022-12-12 22:07:55 -0800

    added get and post routes; added middleware files

--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,38 @@
 const express = require('express');
 const router = express.Router();
 
+//Importing User Model
+const User = require('../models').User
+
+//Importing Middleware for Handler & Authentication
 
 
+//User Routes
+//An Authenticated Route for All Properties and Values for the Authenticated User
+router.get('/users', (req, res) => {
+    //Get the user from the request body.
+    const user = req.currentUser;
+    res.json({
+        id: user.id,
+        emailAddress: user.emailAddress,
+        firstName: user.firstName,
+        lastName: user.lastName,
+    });
+});
+
+// Set the status to 201 Created and end the response 
+res.status(201).end();
+
+//Route that Creates A New User
+router.post('/users', async(req, res) => {
+    try {
+        ...
+    } catch (error) {
+        if (error.name === 'SequelizeValidationError') {
+            const errors = error.errors.map(err => err.message);
+            res.status(400).json({ errors });
+        } else {
+            throw error;
+        }
+    }
+}));
