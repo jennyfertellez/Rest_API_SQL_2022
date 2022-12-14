85aa9d6ec42793b6bcecfcf842d98ddb1e5eaac9
jennyfertellez <jennyfertellez@gmail.com>
2022-12-13 20:47:25 -0800

    Update courses.js

--- routes/courses.js
+++ routes/courses.js
@@ -11,6 +11,29 @@ const { asyncHandler } = require('../middleware/async-handler');
 const { authenticateUser } = require('../middleware/auth-user');
 
 //Route that returns all courses and return a 200 HTTP status code
+router.get('/courses', asyncHandler(async(req, res) => {
+    const courses = await Course.findAll({
+        attributes: [
+            'id',
+            'title',
+            'description',
+            'estimatedTime',
+            'materialsNeeded',
+        ],
+        include: [
+            {
+                model: User,
+                as: 'user',
+                attributes: [
+                    'firstName',
+                    'lastName',
+                    'emailAddress'
+                ],
+            }
+        ]
+    }); 
+    res.status(200).json(courses);
+}));
 
 //Route that returns the corresponding course and return a 200 HTTP status code
 
