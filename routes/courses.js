7c4efae58262f1e9719623fb6707b8da3bbf6a9f
jennyfertellez <jennyfertellez@gmail.com>
2022-12-14 23:58:52 -0800

    Update courses.js

--- routes/courses.js
+++ routes/courses.js
@@ -36,9 +36,93 @@ router.get('/courses', asyncHandler(async(req, res) => {
 }));
 
 //Route that returns the corresponding course and return a 200 HTTP status code
+router.get('/courses', authenticateUser, async(req, res) => {
+    const course = await Course.findByPk(req.params.id, {
+        attributes: [
+            'id',
+            'title',
+            'description',
+            'estimatedTime',
+            'materialsNeeded',
+        ],
+        included: [
+            {
+                model: User,
+                as: 'user',
+                attributes: [
+                    'id',
+                    'firstName',
+                    'lastName',
+                    'emailAddress'
+                ],
+            }
+        ]
+    });
+    res.json(course);
+});
 
 //Route that will create a new course and return a 201 HTTP status code
+router.post('/courses', authenticateUser, async(req, res) => {
+    try { 
+        const user = req.currentUser;
+
+        const course = await Course.create({
+            title: req.body.title,
+            description: req.body.description,
+            estimatedTime: req.body.estimatedTime,
+            materialsNeeded: req.body.materialsNeeded,
+            userId: user.id,
+        });
+    
+        res.status(201)
+        .location(`courses/${course.id}`)
+        .end();
+    } catch (error) {
+        if(error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+            const errors = error.errorr.map(err => err.message);
+            res.status(400).json({ errors });
+        } else {
+            throw error;
+        }
+    }});
 
 //Route that will update the corresponding course and return a 204 status code
+router.put('/courses/:id', authenticateUser, async(req, res) => {
+    try {
+        const user = req.currentUser;
+        const course = await Course.findByPk(req.params.id);
+        if (course.userId === user.id){
+            await course.update(req.body)
+            res.status(204).end()
+        } else {
+            res.status(403)
+            .json({message: `Access Denied. Only the owner of ${course.title} can update the course.`})
+            .end();
+        }
+    } catch (error) {
+        if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError'){
+            const errors = error.errors.map(err => err.message);
+            res.status(400).json({ errors });
+        } else {
+            throw error;
+        }
+    }
+});
 
 //Route that will delete the corresponding course and a return a 204 HTTP status code
+router.delete('courses/:id', authenticateUser, async(req, res) => {
+    const user = req.currentUser;
+    const course = await Course.findByPk(req.params.id);
+
+    if (course.userId === user.id){
+        await course.destroy()
+        res.status(204)
+        .end()
+    } else {
+        res.status(403)
+        .json({message: `Forbidden: ${course.title} can only be deleted by the course owner.`})
+        .end()
+    }
+});
+
+module.exports = router;
